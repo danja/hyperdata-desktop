79db83bba02ec3d37d1890c418671e5c9b60c13b
Danny Ayers <danny.ayers@gmail.com>
2023-09-22 16:38:21 +0200

    adding stuff

---
+++ src/hkms/http-server/server.js
@@ -0,0 +1,18 @@
+var static = require('node-static');
+var file = new static.Server(`${__dirname}/../`)
+
+const log = require('electron-log');
+
+log.info('Hello, log');
+log.info(`${__dirname}`);
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        log.info('in listen');
+        // log.info(request);
+        //  log.info(request.url);
+        file.serve(request, response)
+    }).resume()
+}).listen(8404)
+// /home/danny/HKMS/hkms-desktop/src/hkms/http-server
+// 
